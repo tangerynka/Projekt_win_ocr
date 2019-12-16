f822790a8dd5e19c59e60bba3ff849c96797c1a8
tangerynka <juliak_98@o2.pl>
2019-12-16 09:12:15 +0100

    Dodaj pliki projektów.

---
+++ Projekt_win_ocr/MyModel.cs
@@ -0,0 +1,73 @@
+﻿using IronOcr;
+using IronPdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_win_ocr.MyModelNS
+{
+    class MyModel
+    {
+        public Dictionary<string, IronOcr.Languages.IOcrLanguagePack> languages = new Dictionary<string, IronOcr.Languages.IOcrLanguagePack>()
+        {
+            {"English", IronOcr.Languages.English.OcrLanguagePack },
+            {"Korean", IronOcr.Languages.Korean.OcrLanguagePack },
+            {"Polish", IronOcr.Languages.Polish.OcrLanguagePack }
+        };
+
+        public string ConvertPDF(string path, bool adv, bool bsc, bool _cleanBackgroundNoise, bool _enhanceContrast, bool _enhanceResolution, string language, bool _detectWhiteTextOnDarkBackgrounds, bool _rotateAndStraighten, bool _readBarCodes)
+        {
+            if (bsc)
+            {
+                return ConvertBasic(path, language);
+            }
+            else if (adv)
+            {
+                return ConvertAdvanced(path, _cleanBackgroundNoise, _enhanceContrast, _enhanceResolution, language, _detectWhiteTextOnDarkBackgrounds, _rotateAndStraighten,_readBarCodes);
+            }
+            else return null;
+            
+        }
+
+        private string ConvertAdvanced(string path, bool _cleanBackgroundNoise, bool _enhanceContrast, bool _enhanceResolution, string language, string strategy, bool _detectWhiteTextOnDarkBackgrounds, bool _rotateAndStraighten, bool _readBarCodes)
+        {
+            var Ocr = new IronOcr.AdvancedOcr()
+            {
+                CleanBackgroundNoise = _cleanBackgroundNoise,
+                EnhanceContrast = _enhanceContrast,
+                EnhanceResolution = _enhanceResolution,
+                Language = languages[language],
+                Strategy = IronOcr.AdvancedOcr.OcrStrategy.Advanced,
+                ColorSpace = AdvancedOcr.OcrColorSpace.Color,
+                DetectWhiteTextOnDarkBackgrounds = _detectWhiteTextOnDarkBackgrounds,
+                InputImageType = AdvancedOcr.InputTypes.AutoDetect,
+                RotateAndStraighten = _rotateAndStraighten,
+                ReadBarCodes = _readBarCodes,
+                ColorDepth = 4
+            };
+            var testDocument = path;
+            var Result = Ocr.Read(testDocument);
+            return Result.Text;
+        }
+
+        private string ConvertBasic(string path, string language)
+        {
+            var Ocr = new IronOcr.AutoOcr();
+            Ocr.Language = languages[language];
+            var testDocument = path;
+            var Result = Ocr.Read(testDocument);
+
+            return Result.Text;
+        }
+
+        internal void SavePDF(string outPath, string outFile, string ReadText)
+        {
+            IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
+            // Render an HTML document or snippet as a string
+            Renderer.RenderHtmlAsPdf(ReadText).SaveAs(outPath+outFile+".pdf");
+
+        }
+    }
+}
---
+++ Projekt_win_ocr/MyViewModel.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Projekt_win_ocr.MyViewModelNS
+{
+    using MyModelNS;
+    using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+    public class MyViewModel: INotifyPropertyChanged
+    {
+        private MyModel model = new MyModel();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand FileButtonClick{ get; set; }
+        public ICommand DirButtonClick{ get; set; }
+        public ICommand ConvertButtonClick{ get; set; }
+        private string _path;
+        private string _outPath;
+        private string _outFile;
+        private string _readText;
+        private bool _basicMode = true;
+        private bool _advancedMode;
+        
+        private bool _cleanBackgroundNoise;
+        private bool _enhanceContrast;
+        private bool _enhanceResolution;
+        private bool _detectWhiteTextOnDarkBackgrounds;
+        private bool _rotateAndStraighten;
+        private bool _readBarCodes;
+        private string _language = "English";
+        private string _strategy = "Advanced";
+        
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                NotifyPropertyChanged("Path");
+            }
+        }
+        public string OutPath
+        {
+            get { return _outPath; }
+            set
+            {
+                _outPath = value;
+                NotifyPropertyChanged("OutPath");
+            }
+        }
+        public string OutFile
+        {
+            get { return _outFile; }
+            set
+            {
+                _outFile = value;
+                NotifyPropertyChanged("OutFile");
+            }
+        }
+        public string ReadText
+        {
+            get { return _readText; }
+            set
+            {
+                _readText = value;
+                NotifyPropertyChanged("ReadText");
+            }
+        }
+        public bool BasicMode
+        {
+            get { return _basicMode; }
+            set
+            {
+                _basicMode = value;
+                NotifyPropertyChanged("BasicMode");
+            }
+        }
+        public bool AdvancedMode
+        {
+            get { return _advancedMode; }
+            set
+            {
+                _advancedMode = value;
+                NotifyPropertyChanged("AdvancedMode");
+            }
+        }
+
+        public bool CleanBackgroundNoise
+        {
+            get { return _cleanBackgroundNoise; }
+            set
+            {
+                _cleanBackgroundNoise = value;
+                NotifyPropertyChanged("CleanBackgroundNoise");
+            }
+        }
+        public bool EnhanceContrast
+        {
+            get { return _enhanceContrast; }
+            set
+            {
+                _enhanceContrast = value;
+                NotifyPropertyChanged("EnhanceContrast");
+            }
+        }
+        public bool EnhanceResolution
+        {
+            get { return _enhanceResolution; }
+            set
+            {
+                _enhanceResolution = value;
+                NotifyPropertyChanged("EnhanceResolution");
+            }
+        }
+        public bool DetectWhiteTextOnDarkBackgrounds
+        {
+            get { return _detectWhiteTextOnDarkBackgrounds; }
+            set
+            {
+                _detectWhiteTextOnDarkBackgrounds = value;
+                NotifyPropertyChanged("DetectWhiteTextOnDarkBackgrounds");
+            }
+        }
+        public bool RotateAndStraighten
+        {
+            get { return _rotateAndStraighten; }
+            set
+            {
+                _rotateAndStraighten = value;
+                NotifyPropertyChanged("RotateAndStraighten");
+            }
+        }
+        public bool ReadBarCodes
+        {
+            get { return _readBarCodes; }
+            set
+            {
+                _readBarCodes = value;
+                NotifyPropertyChanged("ReadBarCodes");
+            }
+        }
+        public string Language
+        {
+            get { return _language; }
+            set
+            {
+                _language = value;
+                NotifyPropertyChanged("Language");
+            }
+        }
+        public string Strategy
+        {
+            get { return _strategy; }
+            set
+            {
+                _strategy = value;
+                NotifyPropertyChanged("Strategy");
+            }
+        }
+        public MyViewModel()
+        {
+            FileButtonClick= new RelayCommand(o => FileButtonClk("FileButton"));
+            DirButtonClick= new RelayCommand(o => DirButtonClk("DirButton"));
+            ConvertButtonClick= new RelayCommand(o => ConvertButtonClk("ConvertButton"));
+        }
+        protected void NotifyPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+        
+        private void FileButtonClk(object sender)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Path = openFileDialog.FileName;
+                OutPath = openFileDialog.FileName.Substring(0, openFileDialog.FileName.LastIndexOf("\\")+1);
+                OutFile = "OCR_" + openFileDialog.FileName.Substring(openFileDialog.FileName.LastIndexOf("\\") + 1, openFileDialog.FileName.LastIndexOf(".pdf")-3); //TODO
+            }
+        }
+        private void DirButtonClk(object sender)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    OutPath = fbd.SelectedPath;
+                }
+            }
+        }
+        private void ConvertButtonClk(object sender)
+        {
+            ReadText = model.ConvertPDF(Path, AdvancedMode, BasicMode, CleanBackgroundNoise, EnhanceContrast, EnhanceResolution, Language, DetectWhiteTextOnDarkBackgrounds, RotateAndStraighten, ReadBarCodes);
+            model.SavePDF(OutPath, OutFile, ReadText);
+        }
+
+    }
+}
